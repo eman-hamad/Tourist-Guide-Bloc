501b3ce5d92077589c65d8e0cda6382786b79e27
Eman Hamad <45310369+eman-hamad@users.noreply.github.com>
2025-02-03 01:27:58 +0200

    Update profile_event.dart

--- lib/bloc/profile_bloc/profile_event.dart
+++ lib/bloc/profile_bloc/profile_event.dart
@@ -3,10 +3,25 @@ part of 'profile_bloc.dart';
 // profile events
 abstract class ProfileEvent {}
 
-class LoadSavedImage extends ProfileEvent {}
-
-class LoadProfile extends ProfileEvent {}
-
 class UpdateAvatar extends ProfileEvent {}
 
 class LoadHeaderData extends ProfileEvent {}
+
+class SubscribeProfile extends ProfileEvent {}
+
+class ProfileUpdated extends ProfileEvent {
+  final FSUser user;
+
+  ProfileUpdated({required this.user});
+}
+
+class ImageUpdated extends ProfileEvent {
+  final File image;
+
+  ImageUpdated({required this.image});
+}
+
+class ProfileSubscriptionError extends ProfileEvent {
+  final String errorMessage;
+  ProfileSubscriptionError(this.errorMessage);
+}
